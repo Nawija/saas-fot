317b41383f3c4bc51c0916c5d017c96936e08089
Valkon <96625039+Nawija@users.noreply.github.com>
2025-10-28 21:14:49 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/navbar/Header";
 import AppToaster from "@/components/ui/AppToaster";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
-    title: "SaaS",
+    title: "Seovileo",
     description: "",
 };
 
