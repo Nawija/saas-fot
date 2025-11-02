fa2d720a3df37eb46ae78e7ed2c13b8baa0ed6c2
Valkon <96625039+Nawija@users.noreply.github.com>
2025-11-03 00:37:23 +0100

    <Analytics/>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import AppToaster from "@/components/ui/AppToaster";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
     title: "Seovileo",
@@ -15,6 +16,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={` antialiased`}>
+                <Analytics/>
                 <AppToaster />
                 <main>{children}</main>
             </body>
